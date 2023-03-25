a903e6f8573c233ef5e659acb2128fee803b19bc
Somnath Bera <somnathbera1919@gmail.com>
2023-03-25 09:41:11 +0530

    added timestamp in console output

--- ConsoleOutput.java
+++ ConsoleOutput.java
@@ -1,5 +1,16 @@
+
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class ConsoleOutput {
+    public static void main(String[] args) {
+        printSeparator(20);
+    }
     public static void printSeparator(int length){
-        System.out.println("#".repeat(length));
+        System.out.println("#".repeat(length) + " "+ getCurrentTime());
+    }
+    private static  String getCurrentTime(){
+        return (new SimpleDateFormat("HH:mm:ss.SSS")).format(new Timestamp(System.currentTimeMillis()));
     }
 }
